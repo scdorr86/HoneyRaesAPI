1683fd9533eb28b9870ef35a0fd2899873f7ea88
Sean Dorris <seanc.dorris@gmail.com>
2023-08-19 12:06:45 -0500

    put endpoint to update employee

--- Program.cs
+++ Program.cs
@@ -297,6 +297,23 @@ app.MapDelete("/customers/{id}", (int id) =>
     return Results.Ok(customers);
 });
 
+app.MapPut("/employees/{id}", (int id, Employee employee) =>
+{
+    Employee employeeToUpdate = serviceTickets.FirstOrDefault(st => st.Id == id);
+    int ticketIndex = serviceTickets.IndexOf(ticketToUpdate);
+    if (ticketToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+    //the id in the request route doesn't match the id from the ticket in the request body. That's a bad request!
+    if (id != serviceTicket.Id)
+    {
+        return Results.BadRequest();
+    }
+    serviceTickets[ticketIndex] = serviceTicket;
+    return Results.Ok(employees);
+});
+
 app.MapPut("/servicetickets/{id}", (int id, ServiceTicket serviceTicket) =>
 {
     ServiceTicket ticketToUpdate = serviceTickets.FirstOrDefault(st => st.Id == id);
